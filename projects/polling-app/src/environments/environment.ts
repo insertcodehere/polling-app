0e8563cd0b586e3753d633f08c76a8a4c9cec743
David Fontes <david.fontes@redshift.pt>
2023-05-29 22:52:03 +0100

    add environment files

---
+++ projects/polling-app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  pollApi: 'https://kube-demo.redscan.global/polling-server/api',
+  pollWs: 'wss://kube-demo.redscan.global/polling-server/api/ws'
+};
